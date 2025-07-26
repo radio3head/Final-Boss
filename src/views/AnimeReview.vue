538fc842559c384390644829a762317dce468bb1
Menzarari A <menzarari.alexandra.axela@gmail.com>
2025-07-26 17:51:28 +0300

    WIP

---
+++ src/views/AnimeReview.vue
@@ -0,0 +1,512 @@
+<script></script>
+<template>
+  <div class="container">
+    <section>
+      <div class="polo">
+        <div class="vizionare">
+          <div class="img-anime">
+            <img src="\public\poze\image 30 (5).png" />
+            <div class="favorite">
+              <svg
+                width="16"
+                height="20"
+                viewBox="0 0 16 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2.00977 0C0.914311 0 0.00976562 0.902599 0.00976562 1.99805L0 20L8 17L16 20V18.5566V2C16 0.906937 15.0931 0 14 0H2.00977ZM2.00977 2H14V17.1133L8 14.8633L2.00195 17.1133L2.00977 2Z"
+                  fill="white"
+                />
+              </svg>
+            </div>
+            <div class="rating">
+              <div class="star no-color">
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+              </div>
+              <div class="star color">
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+                <svg
+                  width="56"
+                  height="55"
+                  viewBox="0 0 56 55"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M28.015 0L34.5293 20.7295H55.6099L38.5553 33.541L45.0696 54.2705L28.015 41.459L10.9604 54.2705L17.4747 33.541L0.420095 20.7295H21.5007L28.015 0Z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <button>Vizionare</button>
+        </div>
+        <div class="info">
+          <p>Episoade</p>
+          <p>1100</p>
+        </div>
+      </div>
+
+      <div class="info-anime">
+        <h1>One Piece</h1>
+        <p>
+          Luffy, un băiat cu corp elastic, pornește într-o călătorie plină de aventuri pentru a
+          descoperi comoara „One Piece” și a deveni Regele Piraților. În drumul său, își formează o
+          echipă loială și înfruntă inamici redutabili. Anime-ul impresionează prin construcția
+          lumii, umor, dramă și evoluția profundă a personajelor.
+        </p>
+        <br />
+        <hr />
+        <br />
+        <p class="com">Comentarii:</p>
+
+        <div class="comenatrii">
+          <div class="comment-content">
+            <img class="user-profile" src="/poze/image 44.jpg" />
+
+            <div class="comment-info">
+              <p class="username-text">@adkl7.789:</p>
+              <p class="comment-text">Un univers atât de vast încât simți că trăiești acolo.</p>
+            </div>
+          </div>
+          <div class="comment-content">
+            <img class="user-profile" src="/poze/image45.png" />
+
+            <div class="comment-info">
+              <p class="username-text">@animefannnn23:</p>
+              <p class="comment-text">Luffy e genul de erou care nu renunță niciodată.”</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="comment">
+          <div class="comment2">
+            <img src="/poze/image 46.png" />
+            <img src="/poze/image 47.png" class="smile" />
+            <input
+              type="text"
+              name="comment"
+              placeholder="Adaugă un comentariu:"
+              class="comment-input"
+            />
+            <img src="/poze/image 45.png" />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Poetsen+One&display=swap');
+
+body {
+  font-family: 'Nunito';
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  justify-content: center;
+  margin-top: 41.5px;
+}
+.navbar li {
+  display: inline-block;
+}
+.navbar a {
+  display: flex;
+  flex-direction: row-reverse;
+  float: left;
+  display: block;
+  color: rgb(255, 255, 255);
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+.navbar a:hover {
+  background-image: linear-gradient(black, red, black);
+  color: rgb(255, 255, 255);
+  gap: 8px;
+  border-radius: 8px;
+  padding: 8px;
+}
+#boy {
+  display: flex;
+  margin-right: 58px;
+  width: 50px;
+  height: 53px;
+  background-color: #ffffff;
+  border-radius: 360px;
+}
+.search {
+  background-color: transparent;
+}
+input[type='text'] {
+  box-sizing: border-box;
+  border-radius: 10px;
+  color: #efdfe0;
+  font-size: 16px;
+  background-image: url('girlsimg/search.png');
+  background-repeat: no-repeat;
+  padding: 10px 18px 12px 30px;
+  margin-left: 58px;
+}
+header {
+  display: flex;
+  justify-content: center;
+}
+.cautare {
+  display: flex;
+  margin-top: 45px;
+  margin-bottom: 45px;
+  align-items: center;
+}
+.cautare img {
+  width: 48px;
+  height: 43px;
+  margin-left: 15px;
+  margin-right: 15px;
+}
+#me {
+  border-radius: 10px;
+}
+.buttons img {
+  float: inline-start;
+  width: 30px;
+  height: 30px;
+  margin: 1px;
+}
+#ne {
+  background-image: linear-gradient(black, red, black);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Nunito', sans-serif;
+}
+
+p {
+  font-family: 'Nunito', sans-serif;
+}
+
+body {
+  background: linear-gradient(to bottom, #1a0000, #660000, #1a0000);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+
+.container {
+  width: 1200px;
+  margin: auto;
+  margin-top: 30px;
+}
+
+.vizionare {
+  width: 408px;
+  height: 574px;
+  background-color: white;
+  border-radius: 16px;
+}
+
+.polo {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 408px;
+  height: 647px;
+}
+
+.img-anime {
+  border-radius: 8px 8px 0 0;
+  overflow: hidden;
+  position: relative;
+}
+
+.img-anime img {
+  width: 408px;
+  height: 509px;
+  object-fit: cover;
+  object-position: center center;
+}
+
+.vizionare button {
+  width: 408px;
+  background-color: white;
+  border: none;
+  padding-top: 10px;
+  border-radius: 0 0 16px 16px;
+  color: black;
+  font-size: 32px;
+  font-weight: 700;
+}
+
+.info {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 408px;
+}
+
+.info p {
+  color: white;
+  border-radius: 16px;
+  width: 196px;
+  height: 63px;
+  text-align: center;
+  padding: 15px 0;
+  border: 1px solid white;
+  font-size: 24px;
+  font-weight: 500;
+  background-color: #d9d9d933;
+}
+
+.rating {
+  position: absolute;
+  bottom: 0;
+  height: 254px;
+  background: linear-gradient(180.07deg, rgba(0, 0, 0, 0) 48.2%, rgba(0, 0, 0, 0.89) 99.94%);
+  width: 408px;
+}
+
+.star {
+  position: absolute;
+  bottom: 34px;
+  display: flex;
+  justify-content: space-between;
+  width: 353px;
+  left: 19px;
+}
+
+.no-color svg {
+  fill: white;
+}
+
+.color svg {
+  fill: #ddc55a;
+}
+
+.favorite {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 40px;
+  height: 40px;
+  background: var(--Color-1, #2c0203);
+
+  padding: 10px 12px;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+section {
+  display: flex;
+  justify-content: space-between;
+}
+
+.info-anime {
+  width: 706px;
+}
+
+.info-anime h1 {
+  font-family: 'Poetsen One', sans-serif;
+  font-size: 86px;
+  font-weight: 400;
+  color: white;
+  padding: 20px;
+}
+.info-anime p {
+  font-size: 16px;
+  color: white;
+  padding: 0 20px;
+  font-family: 'Nunito', sans-serif;
+}
+
+.info-anime .com {
+  font-family: 'Poetsen One', sans-serif;
+  font-size: 16px;
+}
+
+.comment-content {
+  display: flex;
+  background-color: #b004054d;
+  border-radius: 20px;
+  padding: 10px;
+}
+
+.user-profile {
+  width: 65px;
+  height: 57px;
+  border-radius: 5px;
+}
+
+.username-text {
+  font-size: 16px;
+  font-weight: 200;
+  color: white;
+}
+
+.comment-text {
+  color: white;
+  font-weight: 400;
+  margin-top: 16px;
+}
+
+.comment-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: first baseline;
+}
+
+.comenatrii {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-bottom: 50px;
+}
+
+.comment2 {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 50px;
+  padding: 10px;
+  font-size: 16px;
+}
+
+.comment {
+  height: 160px;
+  background-color: #b004054d;
+  color: white;
+  border: none;
+  border-radius: 20px;
+}
+
+.comment-input {
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  border: none;
+  color: white;
+  font-size: 16px;
+  padding-left: 15px;
+  border: none;
+}
+
+.smile {
+  width: 30px;
+  height: 30px;
+  margin-left: 10px;
+}
+
+footer {
+  height: 400px;
+}
+</style>
